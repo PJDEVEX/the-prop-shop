cc0db4b632855441400466ea70aa54b692c42a78
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-09-20 20:17:01 +0000

    Changed project name to drf_api

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -34,7 +34,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['8000-pjdevex-thepropshop-fhncw5hdrsb.ws-eu104.gitpod.io']
 
 
 # Application definition
