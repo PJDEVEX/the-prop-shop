4fa9bbb81b7d22a5dd0c49069b715cff5b2f253a
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-10-12 20:56:57 +0000

    Update with added fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,10 +3,17 @@ from .models import Account
 
 
 class AccountAdmin(admin.ModelAdmin):
-    list_display = ('email', 'username', 'first_name', 'last_name', 'is_staff', 'is_active',)
-    list_filter = ('is_staff', 'is_active')
-    search_fields = ('email', 'username', 'first_name', 'last_name')
-    ordering = ('email',)
+    list_display = (
+        "email",
+        "username",
+        "first_name",
+        "last_name",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = ("is_staff", "is_active")
+    search_fields = ("email", "username", "first_name", "last_name")
+    ordering = ("email",)
 
 
 # Register the Account model with the custom admin class
