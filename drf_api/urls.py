4b29aa6f1db45f289a88d15f5222c6003eed7fa0
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-10-14 15:14:06 +0000

    Change the order of url paths

--- drf_api/urls.py
+++ drf_api/urls.py
@@ -9,10 +9,10 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     # Django Rest Framework auth
     path("api-auth/", include("rest_framework.urls")),
-    # Uncomment the line below if you want to include a custom HTML template
-    # path("", TemplateView.as_view(template_name="index.html")),
     # Social auth - drf_social_oauth2
     re_path("api-auth/", include('drf_social_oauth2.urls', namespace='drf')),
+    # Custom HTML template
+    #  path("", TemplateView.as_view(template_name="index.html")),
     # Accounts views
     path("api-auth/", include("accounts.urls")),
 ]
