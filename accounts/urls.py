e97dedbaefe20d9843159ad21e2c2c48e634450f
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-10-14 07:57:40 +0000

    feat: Implement owner-based permissions in UserDetail

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("create/", CreateAccount.as_view(), name="create_user"),
     path('users/', AllUsers.as_view(), name="all"),
     path('currentUser/', CurrentUser.as_view(), name="current"),
-    path('users/<int:user_id>/', UserDetail.as_view(), name="user_detail"),
+    path('user/<int:pk>/', UserDetail.as_view(), name="user_detail"),
 ]
