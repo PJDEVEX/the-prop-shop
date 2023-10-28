117fc2061dfd684edd32592dd3a8c166d5470d0b
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-10-28 08:48:53 +0000

    USER STORY: Color-mode #75 - Style toggle

--- frontend/src/components/ColorModeToggle.js
+++ frontend/src/components/ColorModeToggle.js
@@ -4,19 +4,23 @@ import '@theme-toggles/react/css/Classic.css';
 import { Classic } from '@theme-toggles/react';
 
 const ColorModeToggle = () => {
+  // Access the color mode context to get the current mode and the toggle function
   const { isDarkMode, toggleColorMode } = useColorModeContext();
 
+  // Handle the toggle button click
   const handleToggleClick = () => {
     toggleColorMode();
   };
-
+  // Bug print
   console.log('isDarkMode:', isDarkMode);
 
   return (
+    // Classic toggle component
     <Classic
       duration={750}
       toggled={isDarkMode}
       onToggle={handleToggleClick}
+      className='btn btn-lg'
     />
   );
 };
