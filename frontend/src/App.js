558bb2a98e97e2f59631efc3c82e49e7439a1631
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-10-31 21:14:19 +0000

    USER STORY: User Registration #10 -Add sign up page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,8 @@
 import NavBar from './components/NavBar/NavBar';
 import { Container } from "react-bootstrap";
 import { Route, Switch } from "react-router-dom";
+import "./api/axiosDefaults";
+import SignUpForm from './pages/auth/SignUpFrom';
 
 function App() {
   return (
@@ -12,7 +14,7 @@ function App() {
           {/* Route for the pages */}
         <Route path="/" exact render = { () => <h1>Home Page</h1>} />
         <Route path="/login" exact render = { () => <h1>Sign in</h1>} />
-        <Route path="/create" exact render = { () => <h1>Sign up</h1>} />
+        <Route path="/create" exact render = { () => <SignUpForm />} />
         <Route path="/user-favorites/" exact render = { ()=> <h1>My Favorites</h1> }/>
         
         {/* ROute for page not found */}
