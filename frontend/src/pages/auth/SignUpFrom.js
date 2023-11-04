e4f2398c2e1bad61f7a9ff23de0c582a31f9174c
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-04 14:17:53 +0000

    Update SignUpForm component with form handling and social login

--- frontend/src/pages/auth/SignUpFrom.js
+++ frontend/src/pages/auth/SignUpFrom.js
@@ -1,20 +1,55 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import styles from "./SignInUp.module.css";
-import { Form, Button, Container } from "react-bootstrap";
+import { Form, Button, Container, Alert } from "react-bootstrap";
 import { useColorScheme } from "../../hooks/useColorScheme";
+import axios from "axios";
+import SocialLogin from "./../SocialLogin";
 
 const SignUpForm = () => {
-  // Get the dark mode flag using the useColorScheme hook
+  // Determine if the app is in dark mode
   const { isDark } = useColorScheme();
-
-  // Determine the 'dark' class based on the dark mode flag
   const darkClass = isDark ? styles["dark"] : "";
+
+  // State to store user sign-up data and errors
+  const [SignUpData, setSignUpData] = useState({
+    email: "",
+    username: "",
+    first_name: "",
+    password: "",
+  });
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  // Handle changes in form fields
+  const handleChange = (event) => {
+    setSignUpData({
+      ...SignUpData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  // Handle form submission
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // Send a POST request to create a user
+      await axios.post("/api/create/", SignUpData);
+      // Redirect to the sign-in page on successful registration
+      history.push("/login/");
+    } catch (error) {
+      // Set errors if there is a response from the server
+      setErrors(error.response?.data || {});
+      // Log the error to the console for debugging
+      console.error("Error in form submission:", error);
+    }
+  };
 
   return (
     <div className={`${styles.wrapper} ${darkClass}`}>
-      {/* Logo Section */}
       <div className={styles.logo}>
         <img
           className={`${styles.logoImg} ${darkClass}`}
@@ -22,55 +57,104 @@ const SignUpForm = () => {
           alt="The Prop Shop Logo"
         />
       </div>
-
-      {/* Sign Up Title */}
       <div className={`text-center mt-4 ${styles.name} ${darkClass}`}>
         Sign Up
       </div>
 
-      {/* Sign Up Form */}
-      <Form className={`p-3 mt-3 ${darkClass}`}>
-        {/* Username Field */}
+      {/* Social Sign-In */}
+      <SocialLogin />
+
+      <Form onSubmit={handleSubmit} className={`p-3 mt-3 ${darkClass}`}>
+
+        {/* Email Field */}
+        <Form.Group
+          className={`form-field d-flex align-items-center ${styles["form-field"]} ${darkClass}`}
+          controlId="email"
+        >
+          <Form.Label className="d-none">Email</Form.Label>
+          <i className="far fa-envelope"></i>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Email"
+            value={SignUpData.email}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        {errors.email?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        {/* User Name Field */}
         <Form.Group
           className={`form-field d-flex align-items-center ${styles["form-field"]} ${darkClass}`}
           controlId="username"
         >
-          <Form.Label className="d-none">Username</Form.Label>
+          <Form.Label className="d-none">User Name</Form.Label>
           <i className="far fa-user"></i>
-          <Form.Control type="text" name="userName" placeholder="Username" />
+          <Form.Control
+            type="text"
+            name="username"
+            placeholder="User Name"
+            value={SignUpData.username}
+            onChange={handleChange}
+          />
         </Form.Group>
+        {errors.username?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        {/* First Name Field */}
+        <Form.Group
+          className={`form-field d-flex align-items-center ${styles["form-field"]} ${darkClass}`}
+          controlId="firstName"
+        >
+          <Form.Label className="d-none">First Name</Form.Label>
+          <i className="far fa-user"></i>
+          <Form.Control
+            type="text"
+            name="first_name"
+            placeholder="First Name"
+            value={SignUpData.first_name}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        {errors.first_name?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
 
         {/* Password Field */}
         <Form.Group
           className={`form-field d-flex align-items-center ${styles["form-field"]} ${darkClass}`}
-          controlId="password1"
+          controlId="password"
         >
           <Form.Label className="d-none">Password</Form.Label>
           <i className="fa-solid fa-key"></i>
-          <Form.Control type="text" name="password" placeholder="Password" />
-        </Form.Group>
-
-        {/* Confirm Password Field */}
-        <Form.Group
-          className={`form-field d-flex align-items-center ${styles["form-field"]} ${darkClass}`}
-          controlId="password2"
-        >
-          <Form.Label className="d-none">Confirm Password</Form.Label>
-          <i className="fa-solid fa-key"></i>
           <Form.Control
-            type="text"
-            name="password2"
-            placeholder="Confirm Password"
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={SignUpData.password}
+            onChange={handleChange}
           />
         </Form.Group>
+        {errors.password?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
 
-        {/* Sign Up Button */}
         <Button className={`btn mt-3 ${styles.btn} ${darkClass}`} type="submit">
           Sign up
         </Button>
       </Form>
 
-      {/* Link to Sign In */}
       <Container className={`mt-3 ${darkClass}`}>
         <Link className={`${styles.link} ${darkClass}`} to="/signin">
           Already have an account? <span>Sign in</span>
