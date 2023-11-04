aad807458cfce8e4470c77568d93a2368a3aeac0
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-04 16:34:05 +0000

    USER STORY: Sign In #12

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import { Container } from "react-bootstrap";
 import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from './pages/auth/SignUpFrom';
+import SignInForm from './pages/auth/SignInForm'
 
 function App() {
   return (
@@ -13,8 +14,8 @@ function App() {
         <Switch>
           {/* Route for the pages */}
         <Route path="/" exact render = { () => <h1>Home Page</h1>} />
-        <Route path="/login" exact render = { () => <h1>Sign in</h1>} />
-        <Route path="/create" exact render = { () => <SignUpForm />} />
+        <Route path="/login/" exact render = { () => <SignInForm />} />
+        <Route path="/create/" exact render = { () => <SignUpForm />} />
         <Route path="/user-favorites/" exact render = { ()=> <h1>My Favorites</h1> }/>
         
         {/* ROute for page not found */}
---
+++ frontend/src/pages/auth/SignInForm.js
@@ -0,0 +1,125 @@
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import logo from "../../assets/logo.png";
+import styles from "./SignInUp.module.css";
+import { Form, Button, Container, Alert } from "react-bootstrap";
+import { useColorScheme } from "../../hooks/useColorScheme";
+import axios from "axios";
+import SocialLogin from "../SocialLogin";
+
+function SignInForm () {
+  // Determine if the app is in dark mode
+  const { isDark } = useColorScheme();
+  const darkClass = isDark ? styles["dark"] : "";
+
+  // State to store user sign-up data and errors
+  const [signInData, setSignInData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const {email, password} = signInData;
+
+  const [errors, setErrors] = useState({});
+
+  const history = useHistory();
+
+  // Handle changes in form fields
+  const handleChange = (event) => {
+    setSignInData({
+      ...signInData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  // Handle form submission
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      // Send a POST request to create a user
+      await axios.post("api-auth/token", signInData);
+      // Redirect to the sign-in page on successful registration
+      history.push("/");
+    } catch (error) {
+      // Set errors if there is a response from the server
+      setErrors(error.response?.data || {});
+      // Log the error to the console for debugging
+      console.error("Error in form submission:", error);
+    }
+  };
+
+  return (
+    <div className={`${styles.wrapper} ${darkClass}`}>
+      <div className={styles.logo}>
+        <img
+          className={`${styles.logoImg} ${darkClass}`}
+          src={logo}
+          alt="The Prop Shop Logo"
+        />
+      </div>
+      <div className={`text-center mt-4 ${styles.name} ${darkClass}`}>
+        Sign In
+      </div>
+
+      {/* Social Sign-In */}
+      <SocialLogin />
+
+      <Form onSubmit={handleSubmit} className={`p-3 mt-3 ${darkClass}`}>
+
+        {/* Email Field */}
+        <Form.Group
+          className={`form-field d-flex align-items-center ${styles["form-field"]} ${darkClass}`}
+          controlId="email"
+        >
+          <Form.Label className="d-none">Username</Form.Label>
+          <i className="far fa-envelope"></i>
+          <Form.Control
+            type="email"
+            name="email"
+            placeholder="Enter your email"
+            value={signInData.email}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        {errors.email?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        {/* Password Field */}
+        <Form.Group
+          className={`form-field d-flex align-items-center ${styles["form-field"]} ${darkClass}`}
+          controlId="password"
+        >
+          <Form.Label className="d-none">Password</Form.Label>
+          <i className="fa-solid fa-key"></i>
+          <Form.Control
+            type="password"
+            name="password"
+            placeholder="Password"
+            value={signInData.password}
+            onChange={handleChange}
+          />
+        </Form.Group>
+        {errors.password?.map((message, idx) => (
+          <Alert variant="warning" key={idx}>
+            {message}
+          </Alert>
+        ))}
+
+        <Button className={`btn mt-3 ${styles.btn} ${darkClass}`} type="submit">
+          Sign In
+        </Button>
+      </Form>
+
+      <Container className={`mt-3 ${darkClass}`}>
+        <Link className={`${styles.link} ${darkClass}`} to="/create/">
+          Dont have an account? <span>Sign up now!</span>
+        </Link>
+      </Container>
+    </div>
+  );
+};
+
+export default SignInForm;
