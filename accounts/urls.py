a1acd69d60488129be7f3b70cff9aee31da240cc
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-11 21:43:03 +0000

    Refact: all users and current user

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,9 +4,9 @@ from .views import CreateAccount, AllUsers, CurrentUser
 app_name = "users"
 
 urlpatterns = [
-    path("create/", CreateAccount.as_view(), name="create_user"),
-    path('users/', AllUsers.as_view(), name="all_users"),
-    path('currentUser/', CurrentUser.as_view(), name="current_user"),
+    path("create/", CreateAccount.as_view(), name="create-user"),
+    path('all-users/', AllUsers.as_view(), name="all-users"),
+    path('currentUser/', CurrentUser.as_view(), name="current-user"),
     # path('users/<int:pk>/', UserDetail.as_view(), name="user_detail"),
 
 ]
