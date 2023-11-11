a18462ca3d5a2590852494aecac418ff3d81bd54
Piyankara Jayadewa (PJ) <Piyankara.jayadewa@gmail.com>
2023-11-11 21:09:00 +0000

    Remove user_detail

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CreateAccount, AllUsers, CurrentUser, UserDetail
+from .views import CreateAccount, AllUsers, CurrentUser
 
 app_name = "users"
 
@@ -7,6 +7,6 @@ urlpatterns = [
     path("create/", CreateAccount.as_view(), name="create_user"),
     path('users/', AllUsers.as_view(), name="all_users"),
     path('currentUser/', CurrentUser.as_view(), name="current_user"),
-    path('users/<int:pk>/', UserDetail.as_view(), name="user_detail"),
+    # path('users/<int:pk>/', UserDetail.as_view(), name="user_detail"),
 
 ]
